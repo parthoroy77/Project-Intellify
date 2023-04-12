2ed7433958f5d8d773d81dac9bbc35023bcdc6d3
Partho Roy <roypartho772@gmail.com>
2023-04-12 23:16:32 +0600

    chart added

--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { PureComponent } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar } from 'recharts';
+
+
 
 const Statistic = () => {
+    const assignmentData = [
+        { name: 'a-1', result: 60 },
+        { name: 'a-2', result: 60 },
+        { name: 'a-3', result: 60 },
+        { name: 'a-4', result: 60 },
+        { name: 'a-5', result: 55 },
+        { name: 'a-6', result: 60 },
+        { name: 'a-7', result: 60 },
+        { name: 'a-8', result: 60 },
+    ]
     return (
-        <div>
-            
+        <div className=' flex justify-center items-center' style={{height: '700px'}}>
+            <div>
+                <RadarChart
+                width={1000} height={500}    cx="50%" cy="50%" outerRadius="80%" data={assignmentData}>
+                    <PolarGrid />
+                    <Tooltip></Tooltip>
+                    <PolarAngleAxis dataKey="name" />
+                    <PolarRadiusAxis />
+                    <Radar name="Mike" dataKey="result" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                </RadarChart>
+            </div>
         </div>
     );
 };
